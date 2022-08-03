d084c6c4caa75b787363ed360cdd410a8b8f354c
Dsposeidon <51982814+Dsposeidon@users.noreply.github.com>
2022-08-03 00:43:16 -0400

    Add files via upload

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,7 @@
+"""
+This Python file is for the flask server
+"""
+
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -6,19 +10,25 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
+    """ This function uses the translator package on the webpage """
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    frenchText = translator.english_to_french(textToTranslate)
+    return f"Translated text to French: {frenchText}"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
+    """ This function uses the translator package on the webpage """
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    englishText = translator.french_to_english(textToTranslate)
+    return f"Translated text to English: {englishText}"
 
 @app.route("/")
 def renderIndexPage():
+    """ This function renders the webpage template using index.html """
     # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
